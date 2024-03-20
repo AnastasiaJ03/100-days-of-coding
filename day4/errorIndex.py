51f1a9fdc82415468a75d55ffc62fdb1080cb120
Anastasiia <your_email@whatever.com>
2024-03-20 22:47:13 +0200

    treasure map

---
+++ day4/errorIndex.py
@@ -0,0 +1,6 @@
+# dirty_dozen = ["Strawberries" , "Spinach", "Apples", "Grapes", "Celery", "Tomatoes"]
+fruits = ["Strawberies", "Apples", "Grapes"]
+vegetables = ["Spinach", "Celery", "Tomatoes"]
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen[1][1])
